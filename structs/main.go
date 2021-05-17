d5def852ac356e08f6523088a2cb796904eeeb88
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2021-05-17 16:32:33 -0400

    Updating struct values

--- structs/main.go
+++ structs/main.go
@@ -9,7 +9,15 @@ type person struct {
 
 func main() {
 	//alex := person{"Alex", "Anderson"}
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	//alex := person{firstName: "Alex", lastName: "Anderson"}
+	//fmt.Println(alex)
+
+	//3rd way
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
+	fmt.Printf("%+v", alex)
 
 }
