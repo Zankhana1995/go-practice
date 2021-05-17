e060e8b4f58a1438d458209fec7e2f791174752b
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2021-05-17 16:54:14 -0400

    Structs with receiver function

--- structs/main.go
+++ structs/main.go
@@ -10,7 +10,7 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
@@ -18,11 +18,21 @@ func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "Jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
+	// not gonna work updating this way, need to use pointers
+	jim.updateName("Jimmy")
+	jim.print()
 
-	fmt.Printf("%+v", jim)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
